a99723f0910f23214ef33d70d7bb99b7f85d8a28
Tobías Gasparotto Vietto <tobiasgasparotto@gmail.com>
2024-10-08 18:27:33 -0300

    terminé el primer componente que es la barra de búsqueda

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar = () => {
+  const [query, setQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/search?q=${query}`);
+  };
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input
+        type="text"
+        placeholder="Buscar productos"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit">Buscar</button>
+    </form>
+  );
+};
+
+export default SearchBar;
